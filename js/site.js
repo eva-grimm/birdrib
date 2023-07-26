005999ddee0f882f6fc76ddf30068356e4779805
Cadence Eva Custin <eva.custin@gmail.com>
2023-07-26 01:26:21 -0400

    solution and write-up

--- js/site.js
+++ js/site.js
@@ -1,21 +1,18 @@
 function getValues() {
     let palindromeResults = {
         userString: document.getElementById('userString').value.replace(/[^A-Z0-9]/gi, ""),
+        checkForPalindrome(userString) {
+            userString = userString.toLowerCase();
+            let middle = (userString.length % 2 == 1) ? (userString.length - 3) / 2 : (userString.length / 2) - 1;
+            for (let i = 0; i <= middle; i++) {
+                if (userString[i] != userString[userString.length - 1 - i]) return false;
+            }
+            return true;
+        },
         reverseString(userString) {
             let reversedString = '';
             for (let i = userString.length - 1; i >= 0; i--) reversedString += userString[i];
             return reversedString
-        },
-        checkForPalindrome(userString) {
-            userString = userString.toLowerCase();
-            let middle = (userString.length % 2 == 1) ? (userString.length - 3) / 2 : (userString.length / 2) - 1;
-        
-            for (let i = 0; i <= middle; i++) {
-                if (userString[i] != userString[userString.length - 1 - i]) {
-                    return false
-                }
-            }
-            return true;
         }
     }
     displayResults(palindromeResults);
@@ -29,7 +26,7 @@ function displayResults(palindromeResults) {
         document.getElementById('alert').classList.add('alert-success');
     } else {
         document.getElementById('reverse').textContent = palindromeResults.reverseString(palindromeResults.userString);
-        document.getElementById('result').textContent = "Doesn't look like a palindrome to me, does it?";
+        document.getElementById('result').textContent = "Doesn't look like a palindrome";
         document.getElementById('alert').classList.remove('invisible', 'alert-success');
         document.getElementById('alert').classList.add('alert-danger');
     }
